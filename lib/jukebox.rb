e67bd20011722d19b6a360bb14758d09f73f6075
Theo Weller <weller.theodor@gmail.com>
2019-03-05 18:41:28 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -19,18 +19,7 @@ def help
   - exit : exits this program"
 end
 
-def play
-  songs = [
-    "Phoenix - 1901",
-    "Tokyo Police Club - Wait Up",
-    "Sufjan Stevens - Too Much",
-    "The Naked and the Famous - Young Blood",
-    "(Far From) Home - Tiga",
-    "The Cults - Abducted",
-    "Phoenix - Consolation Prizes",
-    "Harry Chapin - Cats in the Cradle",
-    "Amos Lee - Keep It Loose, Keep It Tight"
-  ]
+def play(songs)
   list
   puts "Please enter a song name or number:"
   user_input = gets.chomp
@@ -44,19 +33,8 @@ def play
   end
 end
 
-def list
+def list(songs)
   counter = 1
-  songs = [
-    "Phoenix - 1901",
-    "Tokyo Police Club - Wait Up",
-    "Sufjan Stevens - Too Much",
-    "The Naked and the Famous - Young Blood",
-    "(Far From) Home - Tiga",
-    "The Cults - Abducted",
-    "Phoenix - Consolation Prizes",
-    "Harry Chapin - Cats in the Cradle",
-    "Amos Lee - Keep It Loose, Keep It Tight"
-  ]
   songs.each do |song|
   puts "#{counter}. #{song}"
   counter += 1
