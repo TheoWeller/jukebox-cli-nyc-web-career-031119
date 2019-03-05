8f939dbde30d9e1b344b99bfaee7fc9199d0ad6d
Theo Weller <weller.theodor@gmail.com>
2019-03-05 18:13:46 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,6 +1,6 @@
 #require 'pry'
 
-require_relative "../bin/jukebox"
+
 def help
   puts "I accept the following commands:
   - help : displays this help message
@@ -13,6 +13,17 @@ def play
 end
 
 def list
+  songs = [
+    "Phoenix - 1901",
+    "Tokyo Police Club - Wait Up",
+    "Sufjan Stevens - Too Much",
+    "The Naked and the Famous - Young Blood",
+    "(Far From) Home - Tiga",
+    "The Cults - Abducted",
+    "Phoenix - Consolation Prizes",
+    "Harry Chapin - Cats in the Cradle",
+    "Amos Lee - Keep It Loose, Keep It Tight"
+  ]
   songs.each do |song|
   puts song
 end
