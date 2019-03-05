bfa3972701d7881f24b11c884bc7a8847f0372c7
Theo Weller <weller.theodor@gmail.com>
2019-03-05 18:39:34 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -20,6 +20,17 @@ def help
 end
 
 def play
+  songs = [
+    "Phoenix - 1901",
+    "Tokyo Police Club - Wait Up",
+    "Sufjan Stevens - Too Much",
+    "The Naked and the Famous - Young Blood",
+    "(Far From) Home - Tiga",
+    "The Cults - Abducted",
+    "Phoenix - Consolation Prizes",
+    "Harry Chapin - Cats in the Cradle",
+    "Amos Lee - Keep It Loose, Keep It Tight"
+  ]
   list
   puts "Please enter a song name or number:"
   user_input = gets.chomp
