4cb1db6d1ef2b0301c2cbaf13cf2ee8000958695
Theo Weller <weller.theodor@gmail.com>
2019-03-05 19:12:54 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,13 +10,13 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   user_input = gets.chomp
-  songs.each do |song|
-  if user_input.include?(user_input)
-    puts "Playing #{song}"
+  if (1..9).include?(user_input.to_i)
+   puts "Playing #{songs[user_input.to_i-1]}"
+  elsif songs.include?(user_input)
+    puts "Playing #{user_input}"
   else
-    puts "Invalid input."
-  end
-end
+   puts "Invalid input."
+ end
 end
 
 
