ae00e2b43e902c6c687715591c877d1732776b8f
Theo Weller <weller.theodor@gmail.com>
2019-03-05 18:28:39 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,5 +1,15 @@
 #require 'pry'
-
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
 
 def help
   puts "I accept the following commands:
@@ -11,8 +21,15 @@ end
 
 def play
   "Please enter a song name or number:"
-  user_input = gets.chomp
-  #if user_input ==
+  user_input = gets.chomp.to_i
+  if user_input == integer && (1..9)
+    user_input.to_i
+    puts "Playing #{songs[user_input]}"
+  elsif songs.include(user_input)
+    puts "Playing #{songs[user_input]}"
+  else
+    puts "Invalid input"
+  end
 end
 
 def list
